e72a34db663ea4f9a7a77551997c76a252680c84
Manish Srivastava <mannu927@yahoo.com>
2025-03-15 14:27:58 +0530

    Connection pool move construction, etc

--- src/connection_pool.h
+++ src/connection_pool.h
@@ -21,18 +21,37 @@ namespace common {
         std::chrono::milliseconds cnx_ide_time{0};
     };
 
-    class ConnectionPol {
+    class ConnectionPool {
         Connection _create(const cnx_opt& opts);
         Connection _fetch(std::unique_lock<std::mutex>& lock);
         Connection _fetch();
         void _wait_for_cnx(std::unique_lock<std::mutex>& lock);
+        void _move(ConnectionPool &&that);
 
         cnx_opt cnx_opts;
         pool_opt _pool_opts;
         std::deque<Connection> _pool;
-        std::size_t _used_cnx;
+        std::size_t _used_cnx = 0;
         std::mutex _mutex;
         std::condition_variable _cv;
+
+    public:
+        ConnectionPool(const cnx_opt& opts, const pool_opt& pool_opts);
+        ConnectionPool() = default;
+        ConnectionPool(ConnectionPool&& that);
+        ConnectionPool& operator=(ConnectionPool&& that);
+
+        ConnectionPool(const ConnectionPool& that) = delete;
+        ConnectionPool& operator=(const ConnectionPool& that) = delete;
+
+        ~ConnectionPool() = default;
+
+        //Fetch a connection from pool
+        Connection fetch();
+
+        cnx_opt connection_opts();
+        void release (Connection cnx);
+        Connection create();
     };
 }
 
